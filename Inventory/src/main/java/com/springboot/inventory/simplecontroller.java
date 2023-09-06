02196575852eedada4f56733d3a92511c19ae5a9
kimjongha99 <jjhsun99@gmail.com>
2023-09-06 12:37:06 +0900

    docs: test

--- Inventory/src/main/java/com/springboot/inventory/simplecontroller.java
+++ Inventory/src/main/java/com/springboot/inventory/simplecontroller.java
@@ -9,6 +9,6 @@ public class simplecontroller {
     @GetMapping("/test")
     public String test(){
         System.out.println("asdasd");
-        return "index";
+        return "index";//주석
     }
 }
