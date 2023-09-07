12775e5e60d580aff371872070bb86d5e5540edf
kimjongha99 <jjhsun99@gmail.com>
2023-09-07 12:45:41 +0900

    Create UserRepository.java

---
+++ Inventory/src/main/java/com/springboot/inventory/user/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.springboot.inventory.user.repository;
+
+import com.springboot.inventory.common.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+// 유저 레파지토리 커스텀
+public interface UserRepository extends JpaRepository<User,Long> {
+
+
+}
