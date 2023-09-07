d37eff19a4cd16dfccf14648857547bb88f75b68
kimjongha99 <jjhsun99@gmail.com>
2023-09-07 10:05:08 +0900

    Feat: 초기 세팅 다시함

--- Inventory/src/main/java/com/springboot/inventory/InventoryApplication.java
+++ Inventory/src/main/java/com/springboot/inventory/InventoryApplication.java
@@ -6,8 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InventoryApplication {
 
+<<<<<<< Updated upstream
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryApplication.class, args);
 	}
+=======
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryApplication.class, args);
+    }
+>>>>>>> Stashed changes
 
 }
