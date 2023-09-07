4606e4ccb0ffd2b12ce7d15375e8eeb263e8a99f
kimjongha99 <jjhsun99@gmail.com>
2023-09-07 12:45:29 +0900

    Create UserRoleEnum.java

---
+++ Inventory/src/main/java/com/springboot/inventory/common/enums/UserRoleEnum.java
@@ -0,0 +1,25 @@
+package com.springboot.inventory.common.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum UserRoleEnum {
+    USER(Authority.USER, "유저"), // 사용자 권한
+    ADMIN(Authority.ADMIN, "비품 총괄 관리자"), // 관리자 권한
+    MASTER(Authority.MASTER, "마스터");// 마스터 권한
+
+    private final String authority;
+    UserRoleEnum(String authority, String korean) {
+        this.authority = authority;
+    }
+
+    public String getAuthority() {
+        return this.authority;
+    }
+
+    public static class Authority {
+        public static final String USER = "ROLE_USER";  // 유저
+        public static final String ADMIN = "ROLE_ADMIN";    // 비품 총괄 관리자
+        public static final String MASTER = "ROLE_MASTER";  // 마스터
+    }
+}
