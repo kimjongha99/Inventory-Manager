8b87c55bbd909b7a10652685edf3a72da4a479b2
SUPERK98 <84505087+SUPERK98@users.noreply.github.com>
2023-09-07 11:10:12 +0900

    주석
    주석

--- Inventory/src/main/java/com/springboot/inventory/InventoryApplication.java
+++ Inventory/src/main/java/com/springboot/inventory/InventoryApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InventoryApplication {
-
+	//주석
+	//주석
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryApplication.class, args);
 	}
