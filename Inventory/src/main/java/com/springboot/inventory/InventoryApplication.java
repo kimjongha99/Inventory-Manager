38f934a7563e8f9451b3a563ee5b670307e82404
JinDalgi <dalgi5986@gmail.com>
2023-09-07 11:11:22 +0900

    주석

--- Inventory/src/main/java/com/springboot/inventory/InventoryApplication.java
+++ Inventory/src/main/java/com/springboot/inventory/InventoryApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InventoryApplication {
-
+// dasfsd
     public static void main(String[] args) {
         SpringApplication.run(InventoryApplication.class, args);
     }
