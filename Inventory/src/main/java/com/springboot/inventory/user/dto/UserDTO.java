5a780aa80db1566581849832c54defa1166024c7
kimjongha99 <jjhsun99@gmail.com>
2023-09-07 12:45:37 +0900

    Create UserDTO.java

---
+++ Inventory/src/main/java/com/springboot/inventory/user/dto/UserDTO.java
@@ -0,0 +1,30 @@
+package com.springboot.inventory.user.dto;
+
+import com.springboot.inventory.common.entity.User;
+import com.springboot.inventory.common.enums.UserRoleEnum;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserDTO {
+    private Long User_id;
+    private String username;
+    private String name;
+    private String phone;
+    private String image;
+    private UserRoleEnum role;
+    private Boolean alarm;
+
+    public UserDTO(User user) {
+        this.User_id = user.getUser_id();
+        this.username = user.getUsername();
+        this.name = user.getName();
+        this.phone = user.getPhone();
+        this.image = user.getImage();
+        this.role = user.getRole();
+        this.alarm = user.getAlarm();
+    }
+}
