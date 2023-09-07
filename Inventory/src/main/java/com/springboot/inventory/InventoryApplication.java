8278d54f4a8896fbc52c7efc8bded6482371e6f1
Jihoyakim <138176589+Jihoyakim@users.noreply.github.com>
2023-09-07 11:12:13 +0900

    554654
    165

--- Inventory/src/main/java/com/springboot/inventory/InventoryApplication.java
+++ Inventory/src/main/java/com/springboot/inventory/InventoryApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InventoryApplication {
-
+//5646546546
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryApplication.class, args);
 	}
