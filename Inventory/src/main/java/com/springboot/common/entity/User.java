4d9eecfa21c8167aa0a2ea5eccb6cfc0f2769e06
SUPERK98 <84505087+SUPERK98@users.noreply.github.com>
2023-09-07 11:44:33 +0900

    common.user
    user entity

--- Inventory/src/main/java/com/springboot/common/entity/User.java
+++ Inventory/src/main/java/com/springboot/common/entity/User.java
@@ -1,4 +1,43 @@
 package com.springboot.common.entity;
 
-public class User {
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class User  {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long user_id;
+
+    // 아이디
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    // 이름
+    @Column(nullable = false)
+    private String name;
+
+    private String tel;
+
+//  private String image;
+
+    @Column(nullable = false)
+    private String role;
+
+    @Column(nullable = false)
+    private boolean alarm;
+
+    @Column(nullable = false)
+    private Boolean delete;
+
+
+
 }
