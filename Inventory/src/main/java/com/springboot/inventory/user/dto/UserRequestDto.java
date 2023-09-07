521ce38f24d275650b4d16a5198a335804e1f1a4
kimjongha99 <jjhsun99@gmail.com>
2023-09-07 22:53:35 +0900

    Create UserRequestDto.java

---
+++ Inventory/src/main/java/com/springboot/inventory/user/dto/UserRequestDto.java
@@ -0,0 +1,15 @@
+package com.springboot.inventory.user.dto;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRequestDto {
+}
