7d1c8d7fd94287967c8c470188ff77430d102295
JinDalgi <dalgi5986@gmail.com>
2023-09-08 12:28:51 +0900

    1

---
+++ Inventory/src/main/java/com/springboot/inventory/supply/dto/SupplyRequestDto.java
@@ -0,0 +1,4 @@
+package com.springboot.inventory.supply.dto;
+
+public class SupplyRequestDto {
+}
--- Inventory/src/main/java/com/springboot/inventory/user/service/UserService.java
+++ Inventory/src/main/java/com/springboot/inventory/user/service/UserService.java
@@ -1,4 +1,10 @@
 package com.springboot.inventory.user.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
 }
