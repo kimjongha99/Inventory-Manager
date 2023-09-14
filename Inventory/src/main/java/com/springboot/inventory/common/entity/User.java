a0732aac15ad7262213881cff51b15374f8716e3
SUPERK98 <84505087+SUPERK98@users.noreply.github.com>
2023-09-14 16:39:49 +0900

    updaate

--- Inventory/src/main/java/com/springboot/inventory/common/entity/User.java
+++ Inventory/src/main/java/com/springboot/inventory/common/entity/User.java
@@ -21,43 +21,8 @@ public class User extends Timestamped{
     @Column(nullable = false , unique = true) // 유니크 제약조건 추가 , 낫널
     private String username; // 아이디
 
-    @Column(nullable = false)
-    private String password; //패스워드
-
-    @Column(nullable = false)
-    private String nickname; //이름
-    private String tel; //전화번호
-
-    private boolean alarm;
-
-    private boolean deleted;
-
-    @ManyToOne(cascade = CascadeType.PERSIST)
-    @JoinColumn(name = "team_id")
-    private Team team;
-
     private UserRole role;
 
-    public static User toUser(UserDTO userDTO) { //dto를 엔티티로 변환
-        User user = new User();
-        user.setUsername(userDTO.getUsername());
-        user.setPassword(userDTO.getPassword());
-        user.setNickname(userDTO.getNickname());
-        user.setTel(userDTO.getTel());
-        user.setTeam(userDTO.getTeam());
-        return user;
-    }
-
-    public static User toUpdateUser(UserDTO userDTO) { // id까지 업데이트
-        User user = new User();
-        user.setId((userDTO.getId()));
-        user.setUsername(userDTO.getUsername());
-        user.setPassword(userDTO.getPassword());
-        user.setNickname(userDTO.getNickname());
-        user.setTel(userDTO.getTel());
-        user.setTeam(userDTO.getTeam());
-        return user;
-    }
 
 
 }
