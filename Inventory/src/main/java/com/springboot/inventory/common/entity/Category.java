44387a1183a02bf34265ce39cbbda16bea4a8e22
seongmin1117 <128121051+seongmin1117@users.noreply.github.com>
2023-09-14 11:31:11 +0900

    Feat: 비품 기능 추가
    Supply, Category, LargeCategory 수정 및 추가

--- Inventory/src/main/java/com/springboot/inventory/common/entity/Category.java
+++ Inventory/src/main/java/com/springboot/inventory/common/entity/Category.java
@@ -1,4 +1,26 @@
 package com.springboot.inventory.common.entity;
 
+import com.springboot.inventory.common.enums.LargeCategory;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@SQLDelete(sql = "UPDATE category SET deleted = true WHERE id = ?")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String categoryName;
+
+    @Enumerated(EnumType.STRING)
+    private LargeCategory largeCategory;
+
+    private Boolean deleted;
 }
