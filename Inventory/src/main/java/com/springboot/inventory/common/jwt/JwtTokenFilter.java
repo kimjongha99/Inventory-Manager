b4962a4d2d3d92084829fd836c0e6b5319c9a1e3
Yim119 <110885515+Yim119@users.noreply.github.com>
2023-09-15 09:50:10 +0900

    chore : .gitIgnore 수정
    git 가상 메모리 삭제 후 커밋

--- Inventory/src/main/java/com/springboot/inventory/common/jwt/JwtTokenFilter.java
+++ Inventory/src/main/java/com/springboot/inventory/common/jwt/JwtTokenFilter.java
@@ -22,16 +22,6 @@ public class JwtTokenFilter extends OncePerRequestFilter {
         this.jwtTokenProvider = provider;
     }
 
-    /*
-    doFilterInternal: 이 메소드는 필터의 주요 로직을 수행합니다. 클라이언트의 HTTP 요청이 서버로 들어올 때마다 호출됩니다. 각 요청을 가로채서 JWT 토큰의 추출, 검증 및 인증을 처리합니다.
-
-    HttpServletRequest request: HTTP 요청을 나타내는 객체로, 클라이언트가 서버로 보낸 요청에 대한 정보를 포함합니다. 이를 통해 요청 헤더, 본문 등의 정보를 얻을 수 있습니다.
-
-    HttpServletResponse response: HTTP 응답을 나타내는 객체로, 서버가 클라이언트로 응답할 때 사용됩니다. 이를 통해 응답 상태 코드, 헤더 등을 설정할 수 있습니다.
-
-    FilterChain filterChain: 필터 체인을 통해 다음 필터로 요청을 전달할 수 있습니다. 필터 체인을 통해 요청의 처리 흐름을 제어할 수 있습니다.
-    */
-
     @Override
     protected void doFilterInternal(
             HttpServletRequest req,
