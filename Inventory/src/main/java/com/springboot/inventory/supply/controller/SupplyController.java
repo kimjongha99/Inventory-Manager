5bd864624a57da1e0096c122beab68dccdc7f17c
SUPERK98 <kjmu18@gmail.com>
2023-09-17 23:51:50 +0900

    getlist
    리스트출력

--- Inventory/src/main/java/com/springboot/inventory/supply/controller/SupplyController.java
+++ Inventory/src/main/java/com/springboot/inventory/supply/controller/SupplyController.java
@@ -1,8 +1,9 @@
 package com.springboot.inventory.supply.controller;
 
-import com.springboot.inventory.supply.domain.entity.Supply;
+import com.springboot.inventory.supply.dto.SupplyResponseDto;
 import com.springboot.inventory.supply.service.SupplyService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,14 +13,13 @@ import java.util.List;
 @Controller
 @RequiredArgsConstructor
 public class SupplyController {
+    @Autowired
+    private SupplyService supplyService;
 
-    private final SupplyService supplyService;
-
-    @GetMapping(value = "/")
-    public String list(Model model) {
-        List<Supply> supply = supplyService.supplyList();
-        model.addAttribute("supply", supply);
-        return "/member/login";
+    @GetMapping("/supplies")
+    public String listSupplies(Model model) {
+        List<SupplyResponseDto> supplies = supplyService.getAll(); // Supply 데이터를 가져오는 서비스 메서드를 호출
+        model.addAttribute("supplies", supplies); // 모델에 Supply 리스트를 추가
+        return "supplyList"; // supply-list.html 템플릿을 반환
     }
-
 }
