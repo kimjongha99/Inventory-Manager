c5d5de5a57860f87f738fdca68adf5675a01bdf5
SUPERK98 <kjmu18@gmail.com>
2023-09-19 02:31:15 +0900

    update
    status 오류 해결 및 페이징 기능 구현

--- Inventory/src/main/java/com/springboot/inventory/supply/repository/SupplyRepository.java
+++ Inventory/src/main/java/com/springboot/inventory/supply/repository/SupplyRepository.java
@@ -2,6 +2,8 @@ package com.springboot.inventory.supply.repository;
 
 import com.springboot.inventory.common.entity.Supply;
 import com.springboot.inventory.common.enums.SupplyStatusEnum;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,6 +12,7 @@ import java.util.List;
 @Repository
 public interface SupplyRepository extends JpaRepository<Supply, Long> {
     List<Supply> findByDeletedFalse();
-
     List<Supply> findDistinctByStatusAndDeletedFalse(SupplyStatusEnum status);
+    Page<Supply> findAllByDeletedFalse(Pageable pageable);
+    Page<Supply> findDistinctByStatusAndDeletedFalse(SupplyStatusEnum status, Pageable pageable);
 }
