bb109dc55a1559a7306cea3c7b4f526402b31012
SUPERK98 <84505087+SUPERK98@users.noreply.github.com>
2023-09-20 17:40:31 +0900

    List 완성
    ㅅㅅ

--- Inventory/src/main/java/com/springboot/inventory/supply/repository/SupplyRepository.java
+++ Inventory/src/main/java/com/springboot/inventory/supply/repository/SupplyRepository.java
@@ -15,9 +15,10 @@ import java.util.List;
 @Repository
 public interface SupplyRepository extends JpaRepository<Supply, Long> {
 
+    Page<Supply> findByStatusAndDeletedFalse(SupplyStatusEnum status, Pageable pageable);
     Page<Supply> findAllByDeletedFalse(Pageable pageable);
 
-    Page<Supply> findDistinctByStatusAndDeletedFalse(SupplyStatusEnum status, Pageable pageable);
+    Page<Supply> findDistinctByStatusAndDeletedFalse(@Param("status") SupplyStatusEnum status, Pageable pageable);
 
     @Query("SELECT s FROM Supply s " +
             "LEFT JOIN s.category c " +
@@ -30,6 +31,18 @@ public interface SupplyRepository extends JpaRepository<Supply, Long> {
             "s.deleted = false")
     Page<Supply> searchByKeyword(@Param("keyword") String keyword, Pageable pageable);
 
+    //keyword 와 status를 모두 고려한 로직
+    @Query("SELECT s FROM Supply s " +
+            "LEFT JOIN s.category c " +
+            "LEFT JOIN s.user u " +
+            "WHERE " +
+            "(c.categoryName LIKE %:keyword% OR " +
+            "s.modelName LIKE %:keyword% OR " +
+            "s.serialNum LIKE %:keyword% OR " +
+            "u.username LIKE %:keyword%) AND " +
+            "s.status = :status AND " +
+            "s.deleted = false")
+    Page<Supply> searchByKeywordAndStatus(@Param("keyword") String keyword, @Param("status") SupplyStatusEnum status, Pageable pageable);
 
 }
 
--- Inventory/src/main/java/com/springboot/inventory/supply/service/SupplyResponseDtoService.java
+++ Inventory/src/main/java/com/springboot/inventory/supply/service/SupplyResponseDtoService.java
@@ -2,28 +2,25 @@ package com.springboot.inventory.supply.service;
 
 import com.springboot.inventory.common.entity.Supply;
 import com.springboot.inventory.common.enums.SupplyStatusEnum;
+import com.springboot.inventory.supply.dto.SupplyDetailsDto;
 import com.springboot.inventory.supply.dto.SupplyResponseDto;
 import com.springboot.inventory.supply.repository.SupplyRepository;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 @Service
 public class SupplyResponseDtoService {
 
     private final SupplyRepository supplyRepository;
-    private final ModelMapper modelMapper;
 
     @Autowired
-    public SupplyResponseDtoService(SupplyRepository supplyRepository, ModelMapper modelMapper) {
+    public SupplyResponseDtoService(SupplyRepository supplyRepository) {
         this.supplyRepository = supplyRepository;
-        this.modelMapper = modelMapper;
     }
 
     @Transactional
@@ -38,10 +35,9 @@ public class SupplyResponseDtoService {
         return convertToDtoPage(supplies);
     }
 
-
     // 중복되는 Entity에서 DTO로의 변환 로직을 이 메서드로 분리
     private Page<SupplyResponseDto> convertToDtoPage(Page<Supply> supplies) {
-        return supplies.map(supply -> modelMapper.map(supply, SupplyResponseDto.class));
+        return supplies.map(SupplyResponseDto::fromSupply);
     }
 
     @Transactional
@@ -49,5 +45,26 @@ public class SupplyResponseDtoService {
         Page<Supply> supplies = supplyRepository.searchByKeyword(keyword, pageable);
         System.out.println("Search results: " + supplies.getContent());
         return convertToDtoPage(supplies);
+    }
+
+    // Supply ID를 사용하여 특정 Supply 엔티티 조회
+    @Transactional
+    public Optional<SupplyDetailsDto> getSupplyById(Long supplyId) {
+        return supplyRepository.findById(supplyId)
+                .map(SupplyDetailsDto::fromSupplyDetails);
+    }
+    @Transactional
+    public Page<SupplyResponseDto> searchByKeywordAndStatus(String keyword, SupplyStatusEnum status, Pageable pageable) {
+        Page<Supply> supplies = supplyRepository.searchByKeywordAndStatus(keyword, status, pageable);
+        return convertToDtoPage(supplies);
+    }
+
+    public void deleteSupply(Long id) {
+        // Soft Delete: deleted 필드를 true로 설정
+        Supply supply = supplyRepository.findById(id).orElse(null);
+        if (supply != null) {
+            supply.setDeleted(true);
+            supplyRepository.save(supply);
+        }
     }
 }
