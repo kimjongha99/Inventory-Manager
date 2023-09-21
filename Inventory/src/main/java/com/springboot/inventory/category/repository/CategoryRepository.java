c91ffa2789a128c06112ab0a2c354f9627f3f6ec
SUPERK98 <84505087+SUPERK98@users.noreply.github.com>
2023-09-21 11:37:04 +0900

    category완성

--- Inventory/src/main/java/com/springboot/inventory/category/repository/CategoryRepository.java
+++ Inventory/src/main/java/com/springboot/inventory/category/repository/CategoryRepository.java
@@ -3,6 +3,8 @@ package com.springboot.inventory.category.repository;
 import com.springboot.inventory.common.entity.Category;
 import com.springboot.inventory.common.enums.LargeCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,11 +13,13 @@ import java.util.Optional;
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
-    Optional<Category> findByCategoryName(String categoryName);
+    Optional<Category> findByCategoryNameAndDeletedFalse(String categoryName);
 
-    boolean existsByLargeCategory(LargeCategory largeCategory);
-
+    boolean existsByLargeCategoryAndDeletedFalse(LargeCategory largeCategory);
 
     // LargeCategory와 삭제되지 않은 Category 목록을 검색하는 메서드
     List<Category> findByLargeCategoryAndDeleted(LargeCategory largeCategory, Boolean deleted);
+        @Query("SELECT c.categoryName, COUNT(c.categoryName) FROM Category c WHERE c.deleted = false AND c.largeCategory = :selectedLargeCategory GROUP BY c.categoryName ORDER BY c.categoryName DESC")
+        List<Object[]> countGroupedByLargeCategoryAndCategoryName(@Param("selectedLargeCategory") LargeCategory selectedLargeCategory);
+
 }
