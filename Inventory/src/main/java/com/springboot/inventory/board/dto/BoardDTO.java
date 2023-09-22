eeb7e912b60dc2c50ea99bfd78ffa6750ab70d43
kimjongha99 <jjhsun99@gmail.com>
2023-09-22 17:02:26 +0900

    FIX: 최종세팅 (추가기능구현)

---
+++ Inventory/src/main/java/com/springboot/inventory/board/dto/BoardDTO.java
@@ -0,0 +1,39 @@
+package com.springboot.inventory.board.dto;
+
+import com.springboot.inventory.common.enums.PostStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BoardDTO {
+
+    private Long bno;
+
+    @NotEmpty
+    @Size(min = 3, max = 100)
+    private String title;
+
+    @NotEmpty
+    private String content;
+
+    @NotEmpty
+    private String writer;
+
+    private PostStatus status; // 게시물 상태
+
+    private LocalDateTime created_at;
+
+    private LocalDateTime updated_at;
+
+    private Long replyCount;
+
+}
