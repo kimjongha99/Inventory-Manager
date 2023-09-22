1a7599b50b0b1788226f56af09a6905f24d44a40
kimjongha99 <jjhsun99@gmail.com>
2023-09-22 09:32:12 +0900

    Test:요청 머지

---
+++ Inventory/src/main/java/com/springboot/inventory/board/repository/ReplyRepository.java
@@ -0,0 +1,17 @@
+package com.springboot.inventory.board.repository;
+
+import com.springboot.inventory.common.entity.Reply;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface ReplyRepository extends JpaRepository<Reply, Long> {
+
+    @Query("select r from Reply r where r.board.bno = :bno")
+    Page<Reply> listOfBoard(Long bno, Pageable pageable);
+
+
+    int countByBoard_Bno(Long bno);
+
+}
