f0419ff4b4b431a7b6f3c350ef2d6f7245ec4f62
seongmin1117 <128121051+seongmin1117@users.noreply.github.com>
2023-09-22 09:44:16 +0900

    0922

---
+++ Inventory/src/main/java/com/springboot/inventory/supply/dto/SupplyDto.java
@@ -0,0 +1,34 @@
+package com.springboot.inventory.supply.dto;
+
+import com.springboot.inventory.common.entity.Category;
+import com.springboot.inventory.common.enums.LargeCategory;
+import com.springboot.inventory.common.enums.SupplyStatusEnum;
+import com.springboot.inventory.common.enums.UserRole;
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+public class SupplyDto {
+
+    private String serialNum;
+    private String modelContent;
+    private int amount;
+    private String modelName;
+    private SupplyStatusEnum status;
+
+    //이미지
+    private String image;
+    private String imagePath;
+    private MultipartFile multipartFile;
+
+    //카테고리
+    private Category category;
+    private LargeCategory largeCategory;
+    private String categoryName;
+    private String directCategoryName; //직접입력받는 소분류
+
+    //사용자
+    private Long userId;
+    private UserRole userRole;
+
+}
