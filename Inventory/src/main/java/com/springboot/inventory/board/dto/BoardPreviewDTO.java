36619440c82fcc2bd66ccd4373b44931037d123d
Jihoyakim <138176589+Jihoyakim@users.noreply.github.com>
2023-09-26 00:36:33 +0900

    mypage 수정, basic 권한 설정

--- Inventory/src/main/java/com/springboot/inventory/board/dto/BoardPreviewDTO.java
+++ Inventory/src/main/java/com/springboot/inventory/board/dto/BoardPreviewDTO.java
@@ -1,5 +1,6 @@
 package com.springboot.inventory.board.dto;
 
+import com.springboot.inventory.common.enums.BoardType;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,9 +10,19 @@ public class BoardPreviewDTO {
     private String title;
     private String writer;
 
-
+    // Add board type.
+    private BoardType boardType;
     public BoardPreviewDTO(String title, String writer) {
         this.title = title;
         this.writer = writer;
+    }
+
+    // Add getter and setter for the new field.
+    public BoardType getBoardType() {
+        return boardType;
+    }
+
+    public void setBoardType(BoardType boardType) {
+        this.boardType = boardType;
     }
 }
